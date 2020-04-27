abfc5ba3eff18c348b8768e95d684d9160e81f64
Martin Glusker <martin.glusker@gmail.com>
2020-04-27 16:39:41 -0400

    undo previous change

--- src/ring_of_fire/propel_helper.clj
+++ src/ring_of_fire/propel_helper.clj
@@ -2,7 +2,8 @@
   (:use [clojure.repl])
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]
-            [ring-of-fire.data :refer :all]))
+            [ring-of-fire.data :refer :all]
+            [ring-of-fire.core :refer :all]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Spector Propel Code      ;;
