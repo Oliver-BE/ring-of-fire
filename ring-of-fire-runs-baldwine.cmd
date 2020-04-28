144c295bbcb17a540c62fa8179e43566c2d1f17a
icaruso21 <icaruso21@amherst.edu>
2020-04-27 22:06:18 -0400

    Finished creating condor run files and directories

---
+++ ring-of-fire-runs-baldwine.cmd
@@ -0,0 +1,19 @@
+universe    = vanilla
+notification = error
+notify_user  = obaldwinedwards21@amherst.edu
+initialdir  = /home/obaldwinedwards21/ring-of-fire
+getenv = True
+executable  = run
+
+## Job properties
+output = /home/obaldwinedwards21/ring-of-fire/results/baldwine/0/out
+error  = /home/obaldwinedwards21/ring-of-fire/results/baldwine/0/err
+log    = /home/obaldwinedwards21/ring-of-fire/results/baldwine/0/log
+queue
+
+## Job properties
+output = /home/obaldwinedwards21/ring-of-fire/results/baldwine/1/out
+error  = /home/obaldwinedwards21/ring-of-fire/results/baldwine/1/err
+log    = /home/obaldwinedwards21/ring-of-fire/results/baldwine/1/log
+queue
+
--- ring-of-fire-runs-gluskart.cmd
+++ ring-of-fire-runs-gluskart.cmd
@@ -1,19 +1,19 @@
 universe    = vanilla
 notification = error
-notify_user  = icaruso21@amherst.edu
-initialdir  = /home/icaruso21/ring-of-fire
+notify_user  = mglusker21@amherst.edu
+initialdir  = /home/mglusker21/ring-of-fire
 getenv = True
 executable  = run
 
 ## Job properties
-output = /home/icaruso21/ring-of-fire/results/caruso/0/out
-error  = /home/icaruso21/ring-of-fire/results/caruso/0/err
-log    = /home/icaruso21/ring-of-fire/results/caruso/0/log
+output = /home/mglusker21/ring-of-fire/results/glusker/0/out
+error  = /home/mglusker21/ring-of-fire/results/glusker/0/err
+log    = /home/mglusker21/ring-of-fire/results/glusker/0/log
 queue
 
 ## Job properties
-output = /home/icaruso21/ring-of-fire/results/caruso/1/out
-error  = /home/icaruso21/ring-of-fire/results/caruso/1/err
-log    = /home/icaruso21/ring-of-fire/results/caruso/1/log
+output = /home/mglusker21/ring-of-fire/results/glusker/1/out
+error  = /home/mglusker21/ring-of-fire/results/glusker/1/err
+log    = /home/mglusker21/ring-of-fire/results/glusker/1/log
 queue
 
